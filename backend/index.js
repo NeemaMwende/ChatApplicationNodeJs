114528b9df8b44d4bf9c2439750fd526a00db6f8
NeemaMwende <gracepete12@gmail.com>
2024-07-30 19:41:25 +0300

    updates

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,17 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  7d24fa48-fd91-467e-8a63-8c0e47490498
   const { username } = req.body;
 
   try {
-    const response = await axios.put("https://api.chatengine.io/users/", {})
+    const response = await axios.put("https://api.chatengine.io/users/", 
+      {username: username, secret: username, first_name: username, last_name: username},
+      {headers: {"PRIVATE-KEY": "7d24fa48-fd91-467e-8a63-8c0e47490498"}})
+
+      // return res.status(200).send(response.data);
+      return res.status(response.status).json(response.data);
   } catch (error) {
-    
+    return res.status(error.response.status).json(error.response.data);
   }
   return res.json({ username: username, secret: "sha256..." });
 });
