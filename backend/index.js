f49106ca8f7c49f9a46a888651b768cab399652d
NeemaMwende <gracepete12@gmail.com>
2024-07-30 19:34:58 +0300

    authentications

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
+  7d24fa48-fd91-467e-8a63-8c0e47490498
   const { username } = req.body;
+
+  try {
+    const response = await axios.put("https://api.chatengine.io/users/", {})
+  } catch (error) {
+    
+  }
   return res.json({ username: username, secret: "sha256..." });
 });
 
-app.listen(3001);
+app.listen(3001, () => console.log("Server running on port 3001"));
